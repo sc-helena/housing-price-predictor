c7a8ac4e5d39c034ba723aebd9f6850568c4e842
hschn79 <helena.schneider@outlook.de>
2025-01-30 11:24:22 +0100

    implement data download and first visual representation of the data

---
+++ utils/figures.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import os
+
+def _get_project_dir():
+    # Get the current script file path
+    current_script_path = Path(__file__)
+    # Get the project directory
+    parent_directory = current_script_path.parent.parent
+
+    return os.path.join(parent_directory, "assets/figures")
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(_get_project_dir(), f"{fig_id}.{fig_extension}")
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
